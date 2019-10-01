09c98b04300f3087f2c98c6e71c001e1c5bbd8be
Guillermo Sanchez <49462669+guille2400@users.noreply.github.com>
2019-10-01 12:10:32 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -21,6 +21,31 @@ void lectura()
     archivo.close();
 
 }
+template <typename T>
+void ShellSort(vector<T> vector1, T n)
+{
+    T i, j, k, temp;
+    // Gap 'i' between index of the element to be compared, initially n/2.
+    for(i = n/2; i > 0; i = i/2)
+    {
+        for(j = i; j < n; j++)
+        {
+            for(k = j-i; k >= 0; k = k-i)
+            {
+                // If value at higher index is greater, then break the loop.
+                if(vector1[k+i] >= vector1[k])
+                    break;
+                    // Switch the values otherwise.
+                else
+                {
+                    temp = vector1[k];
+                    vector1[k] = vector1[k+i];
+                    vector1[k+i] = temp;
+                }
+            }
+        }
+    }
+}
 
 template <typename T>
 void quickSort(vector<T> &vectorsito)
