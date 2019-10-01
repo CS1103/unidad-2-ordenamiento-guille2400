bcca1f8d4997e5a1c233dab4159bed8fa36462cb
Guillermo Sanchez <49462669+guille2400@users.noreply.github.com>
2019-10-01 11:41:33 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,2 +1,46 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include <iostream>
+#include <vector>
+#include <fstream>
+using  namespace std;
+
+
+void lectura()
+{   ifstream archivo;
+    string texto;
+    archivo.open("Prueba.txt", ios::in); //Abrimos el archivo en modo lectura
+    if (archivo.fail())
+    {cout<< "No se pudo abrir el archivo";
+        exit(1);
+    }
+
+    while(!archivo.eof()) //Mientras no sea el final el archivo
+    {
+        getline(archivo, texto);
+        cout << texto << endl;
+    }
+    archivo.close();
+
+}
+
+template <typename T>
+void quickSort(vector<T> &vectorsito)
+{
+
+}
+
+template <typename T>
+void imprimir(const vector<T> &vectorsito)
+{
+    for( int ax = 0; ax< vectorsito.size(); ax++)
+    {
+        cout << vectorsito[ax]<< " ";
+    }
+}
+
+
+int main()
+{   lectura();
+
+    system("pause");
+    return 0;
+}
