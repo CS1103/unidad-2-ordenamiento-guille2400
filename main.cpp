2e23649ff77a2930789d8ca213b4e5f2acf634d2
Guillermo Sanchez <49462669+guille2400@users.noreply.github.com>
2019-10-01 13:34:02 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
+
 using  namespace std;
 
+template <typename T>
+auto lectura()
+{
+        ifstream infile("Prueba.txt");
+        T line;
+        vector<T> Numeros;
+        T numero;
+        while (getline(infile, line))
+        {
+            istringstream iss(line);
 
-void lectura()
-{   ifstream archivo;
-    string texto;
-    archivo.open("Prueba.txt", ios::in); //Abrimos el archivo en modo lectura
-    if (archivo.fail())
-    {cout<< "No se pudo abrir el archivo";
-        exit(1);
-    }
+            if (iss >>numero)
+                Numeros.push_back(numero);
+        }
 
-    while(!archivo.eof()) //Mientras no sea el final el archivo
-    {
-        getline(archivo, texto);
-        cout << texto << endl;
-    }
-    archivo.close();
-
+        for (auto it = Numeros.begin(); it != Numeros.end(); ++it)
+            cout << " "<< *it;
+        cout << endl;
 }
 template <typename T>
-void ShellSort(vector<T> vector1, T n)
+auto ShellSort(vector<T> &vector1, T n)
 {
     T i, j, k, temp;
     // Gap 'i' between index of the element to be compared, initially n/2.
@@ -48,10 +51,51 @@ void ShellSort(vector<T> vector1, T n)
 }
 
 template <typename T>
-void quickSort(vector<T> &vectorsito)
+auto swap(T* a, T* b)
 {
-
+    T t = *a;
+    *a = *b;
+    *b = t;
 }
+
+/* Toma el ultimo elemento como pivote, toma los menores al
+ lado izquiero  y los mayores al lado derecho*/
+template <typename T>
+T partition (vector<T> &vector1, T low, T high)
+{
+    T pivote = vector1[high]; // pivote
+    T i = (low - 1); // Index del elemento menor
+
+    for (T j = low; j <= high - 1; j++)
+    {
+        // Si el elemento es menor al pivote
+        if (vector1[j] < pivote)
+        {
+            i++; // Incrementa el index del menor elemento
+            swap(&vector1[i], &vector1[j]);
+        }
+    }
+    swap(&vector1[i + 1], &vector1[high]);
+    return (i + 1);
+}
+
+/* La principal fucion que hara el quicksort */
+template <typename T>
+T quickSort (vector<T> &vector1, T low, T high)
+{
+    if (low < high)
+    {
+        /* pi es el index de la particion */
+        T pi = partition(vector1, low, high);
+
+        /* Separa los elementos ordenados
+        antes de la particion y despues de la particion*/
+        quickSort(vector1, low, pi - 1);
+        quickSort(vector1, pi + 1, high);
+    }
+}
+
+
 
 template <typename T>
 void imprimir(const vector<T> &vectorsito)
@@ -64,8 +108,7 @@ void imprimir(const vector<T> &vectorsito)
 
 
 int main()
-{   lectura();
+{
 
-    system("pause");
     return 0;
 }
